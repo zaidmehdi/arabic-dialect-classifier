07cffcd974760243604baaebb8ff89846492d281
zaidmehdi <mehdizaid02@gmail.com>
2024-02-25 09:26:38 +0100

    remove print

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ def convert_df_to_dataset_dict(df_train:pd.DataFrame, df_test:pd.DataFrame)-> Da
     mapper = {"#2_tweet": "tweet", "#3_country_label": "label"}
     df_train = df_train.rename(columns=mapper)
     df_test = df_test.rename(columns=mapper)
-    print(df_train.columns)
+
     columns_to_keep = ["tweet", "label"]
     train_dataset = Dataset.from_pandas(df_train[columns_to_keep])
     test_dataset = Dataset.from_pandas(df_test[columns_to_keep])
