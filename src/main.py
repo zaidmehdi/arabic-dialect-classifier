ab549664d15b19df482bd19f942525a55a2b96c6
zaidmehdi <mehdizaid02@gmail.com>
2024-02-28 22:28:18 +0100

    creating flask api

--- src/main.py
+++ src/main.py
@@ -1,21 +1,34 @@
 import pickle
 
+from flask import Flask, request, jsonify
 from transformers import AutoTokenizer
 
+app = Flask(__name__)
 
-def classify_arabic_dialect(text:str, model, tokenizer) -> str:
-    text_embeddings = tokenizer(text, padding=True)
-    predicted_class = model.predict(text_embeddings)
+with open("../models/logistic_regression.pkl", "rb") as f:
+        model = pickle.load(f)
 
-    return predicted_class
+tokenizer = AutoTokenizer.from_pretrained("moussaKam/AraBART")
+
+
+@app.route("/classify", methods=["POST"])
+def classify_arabic_dialect():
+    try:
+        data = request.json
+        text = data.get("text")
+        if not text:
+            return jsonify({"error": "No text has been received"}), 400
+         
+        text_embeddings = tokenizer(text, padding=True)
+        predicted_class = model.predict(text_embeddings)
+
+        return jsonify({"class": predicted_class}), 200
+    except Exception as e:
+         return jsonify({"error": str(e)}), 500
 
 
 def main():
-    with open("../models/logistic_regression.pkl", "rb") as f:
-        model = pickle.load(f)
-
-    tokenizer = AutoTokenizer.from_pretrained("moussaKam/AraBART")
-    return
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
