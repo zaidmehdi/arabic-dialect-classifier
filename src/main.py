99757c1083a14b4e31bd337042e2d2ad6b740fc2
zaidmehdi <mehdizaid02@gmail.com>
2024-02-28 22:15:54 +0100

    defining classify_arabic_dialect() function

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 import pickle
 
+from transformers import AutoTokenizer
+
+
+def classify_arabic_dialect(text:str, model, tokenizer) -> str:
+    text_embeddings = tokenizer(text, padding=True)
+    predicted_class = model.predict(text_embeddings)
+
+    return predicted_class
+
 
 def main():
     with open("../models/logistic_regression.pkl", "rb") as f:
         model = pickle.load(f)
-        
+
+    tokenizer = AutoTokenizer.from_pretrained("moussaKam/AraBART")
     return
 
 
