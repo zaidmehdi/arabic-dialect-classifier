9480e34e3546c12d6e096d8e9dbd7c9de669ebe4
zaidmehdi <mehdizaid02@gmail.com>
2024-03-01 00:35:16 +0100

    setting up test class and writing firt test of model output

--- tests/model_tests.py
+++ tests/model_tests.py
@@ -1,1 +1,21 @@
+import requests
 import unittest
+
+
+class TestClassifier(unittest.TestCase):
+    def setUp(self) -> None:
+        self.API_URL = "http://localhost:5000/classify"
+        self.dialects = ['Egypt', 'Iraq', 'Saudi_Arabia', 'Mauritania', 'Algeria', 'Syria',
+        'Oman', 'Tunisia', 'Lebanon', 'Morocco', 'Djibouti','United_Arab_Emirates','Kuwait', 
+        'Libya', 'Bahrain', 'Qatar', 'Yemen', 'Palestine', 'Jordan', 'Somalia', 'Sudan']
+
+    def test_output(self):
+        request_data = {"text": "حاجة حلوة اكيد"}
+        response = requests.post(self.API_URL, json=request_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("class", response.json())
+        self.assertIn(response.json()["class"], self.dialects)
+
+
+if __name__ == "__main__":
+    unittest.main()
