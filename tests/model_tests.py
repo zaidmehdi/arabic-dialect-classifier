16568363563000ea30fe37e2f4074b961211d563
zaidmehdi <mehdizaid02@gmail.com>
2024-03-01 23:05:25 +0100

    test obvious dialects

--- tests/model_tests.py
+++ tests/model_tests.py
@@ -8,6 +8,12 @@ class TestClassifier(unittest.TestCase):
         self.dialects = ['Egypt', 'Iraq', 'Saudi_Arabia', 'Mauritania', 'Algeria', 'Syria',
         'Oman', 'Tunisia', 'Lebanon', 'Morocco', 'Djibouti','United_Arab_Emirates','Kuwait', 
         'Libya', 'Bahrain', 'Qatar', 'Yemen', 'Palestine', 'Jordan', 'Somalia', 'Sudan']
+        self.test_set = {
+            "Egypt": "حضرتك بروح زي كدا؟ على طول النهار ده",
+            "Iraq": "همين: شلون، زين، خوش، هواية، كلش، شقد",
+            "Algeria": "بصح راك فاهم لازم الزيت",
+            "Morocco": "واش نتا خدام ولا لا"
+        }
 
     def test_response(self):
         """Test if the response of the /classify API endpoint is correct"""
@@ -17,6 +23,13 @@ class TestClassifier(unittest.TestCase):
         self.assertIn("class", response.json())
         self.assertIn(response.json()["class"], self.dialects)
 
+    def test_model_output(self):
+        """Test that the model correctly classifies obvious dialects"""
+        for country, text, in self.test_set.items():
+            request_data = {"text": text}
+            response = requests.post(self.API_URL, json=request_data)
+            self.assertEqual(response.json()["class"], country)
 
+    
 if __name__ == "__main__":
     unittest.main()
