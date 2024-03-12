747f8ead8e3e3e461bbde8dd614b6d2035554341
zaidmehdi <mehdizaid02@gmail.com>
2024-03-12 23:43:47 +0100

    customizing layout + adding html

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from transformers import AutoModel, AutoTokenizer
 from .utils import extract_hidden_state
 
 
+# Load model
 models_dir = os.path.join(os.path.dirname(__file__), '..', 'models')
 model_file = os.path.join(models_dir, 'logistic_regression.pkl')
 
@@ -16,9 +17,21 @@ if os.path.exists(model_file):
 else:
     print(f"Error: {model_file} not found.")
 
+# Load html
+html_dir = os.path.join(os.path.dirname(__file__), "templates")
+index_html_path = os.path.join(html_dir, "index.html")
+
+if os.path.exists(index_html_path):
+    with open(index_html_path, "r") as html_file:
+        index_html = html_file.read()
+else:
+    print(f"Error: {index_html_path} not found.")
+
+# Load pre-trained model
 model_name = "moussaKam/AraBART"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 language_model = AutoModel.from_pretrained(model_name)
+
 
 def classify_arabic_dialect(text):
     text_embeddings = extract_hidden_state(text, tokenizer, language_model)
@@ -26,11 +39,20 @@ def classify_arabic_dialect(text):
     
     return predicted_class
 
-demo = gr.Interface(
-    fn=classify_arabic_dialect,
-    inputs=["text"],
-    outputs=["text"],
-)
+
+with gr.Blocks() as demo:
+    gr.HTML(index_html)
+    input_text = gr.Textbox(label="Your Arabic Text")
+    submit_btn = gr.Button("Submit")
+    with gr.Row():
+        first_country = gr.Textbox()
+        second_country = gr.Textbox()
+        third_country = gr.Textbox()
+    submit_btn.click(
+        fn=classify_arabic_dialect, 
+        inputs=input_text, 
+        outputs=[first_country, second_country, third_country])
+    gr.HTML("<p>Checkout the Github Repo:</p>")
 
 
 if __name__ == "__main__":
