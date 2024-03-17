7a5576888b2e5095c3539f711ab91285559367b3
zaidmehdi <mehdizaid02@gmail.com>
2024-03-17 14:43:34 +0100

    label encoding

--- src/model_training.py
+++ src/model_training.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from transformers import AutoTokenizer
 
 from utils import get_datasetdict_object
@@ -17,6 +18,11 @@ def get_dataset(train_path:str, test_path:str):
     df_val = df_val.reset_index(drop=True)
     df_test = pd.read_csv(test_path, sep="\t")
 
+    encoder = LabelEncoder()
+    df_train["#3_country_label"] = encoder.fit_transform(df_train["#3_country_label"])
+    df_val["#3_country_label"] = encoder.transform(df_val["#3_country_label"])
+    df_test["#3_country_label"] = encoder.transform(df_test["#3_country_label"])
+
     return get_datasetdict_object(df_train, df_val, df_test)
 
 
@@ -28,7 +34,7 @@ def main():
     dataset = get_dataset("data/DA_train_labeled.tsv", "data/DA_dev_labeled.tsv")
     dataset = dataset.map(tokenize, batched=True)
     
-    print(dataset)
+    print(set(dataset["train"]["label"]))
 
 
 if __name__ == "__main__":
