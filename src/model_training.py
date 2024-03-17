6b5fdb9d70f7781b5228768ff64bb68262bb30e7
zaidmehdi <mehdizaid02@gmail.com>
2024-03-17 15:16:25 +0100

    moving preprocessing functions to utils

--- src/model_training.py
+++ src/model_training.py
@@ -1,40 +1,20 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModel
 
-from utils import get_datasetdict_object
+from utils import get_dataset
 
 
-model_name = "moussaKam/AraBART"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-
-def get_dataset(train_path:str, test_path:str):
-    df_train = pd.read_csv(train_path, sep="\t")
-    df_train, df_val = train_test_split(df_train, test_size=0.23805, random_state=42, 
-                                        stratify=df_train["#3_country_label"])
-    df_train = df_train.reset_index(drop=True)
-    df_val = df_val.reset_index(drop=True)
-    df_test = pd.read_csv(test_path, sep="\t")
-
-    encoder = LabelEncoder()
-    df_train["#3_country_label"] = encoder.fit_transform(df_train["#3_country_label"])
-    df_val["#3_country_label"] = encoder.transform(df_val["#3_country_label"])
-    df_test["#3_country_label"] = encoder.transform(df_test["#3_country_label"])
-
-    return get_datasetdict_object(df_train, df_val, df_test)
-
-
-def tokenize(batch):
-    return tokenizer(batch["tweet"], padding=True)
+class Model():
+    def __init__(self) -> None:
+        pass
 
 
 def main():
-    dataset = get_dataset("data/DA_train_labeled.tsv", "data/DA_dev_labeled.tsv")
-    dataset = dataset.map(tokenize, batched=True)
+    model_name = "moussaKam/AraBART"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name)
+    dataset = get_dataset("data/DA_train_labeled.tsv", "data/DA_dev_labeled.tsv", tokenizer)
     
-    print(set(dataset["train"]["label"]))
+    print(dataset["train"])
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
 import pickle
 
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
-import torch
 from datasets import DatasetDict, Dataset
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 
 def get_datasetdict_object(df_train, df_val, df_test):
@@ -18,17 +20,31 @@ def get_datasetdict_object(df_train, df_val, df_test):
     train_dataset = Dataset.from_pandas(df_train)
     val_dataset = Dataset.from_pandas(df_val)
     test_dataset = Dataset.from_pandas(df_test)
-    
+
     return DatasetDict({'train': train_dataset, 'val': val_dataset,
                         'test': test_dataset})
 
 
-def extract_hidden_state(input_text, tokenizer, language_model):
-    tokens = tokenizer(input_text, padding=True, return_tensors="pt")
-    with torch.no_grad():
-        outputs = language_model(**tokens)
+def tokenize(batch, tokenizer):
+    return tokenizer(batch["tweet"], padding=True)
 
-    return outputs.last_hidden_state[:,0].numpy()
+
+def get_dataset(train_path:str, test_path:str, tokenizer):
+    df_train = pd.read_csv(train_path, sep="\t")
+    df_train, df_val = train_test_split(df_train, test_size=0.23805, random_state=42, 
+                                        stratify=df_train["#3_country_label"])
+    df_train = df_train.reset_index(drop=True)
+    df_val = df_val.reset_index(drop=True)
+    df_test = pd.read_csv(test_path, sep="\t")
+
+    encoder = LabelEncoder()
+    df_train["#3_country_label"] = encoder.fit_transform(df_train["#3_country_label"])
+    df_val["#3_country_label"] = encoder.transform(df_val["#3_country_label"])
+    df_test["#3_country_label"] = encoder.transform(df_test["#3_country_label"])
+
+    dataset = get_datasetdict_object(df_train, df_val, df_test)
+
+    return dataset.map(lambda x: tokenize(x, tokenizer), batched=True)
 
 
 def serialize_data(data, output_path:str):
